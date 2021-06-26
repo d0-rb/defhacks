ea4967858ecf091e905ed11d562662ea177b2ebb
d0-rb <hacperu2010@gmail.com>
2021-06-26 10:26:39 -0500

    added timeline
    yuhh

--- frontend/x/src/App.js
+++ frontend/x/src/App.js
@@ -1,11 +1,60 @@
 import Meeting from './meeting/Meeting';
+import React from 'react';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
-function App() {
-  return (
-    <div className="App full-size">
-      <Meeting />
-    </div>
-  );
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#64dd17',
+    },
+    secondary: {
+      main: '#64dd17',
+    },
+  },
+});
+
+class App extends React.Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      page: 'main',
+      roomName: '24',
+      displayName: 'bruh',
+      password: 'abc',
+      workout: [
+        {
+          title: 'push-ups',
+          display: 'https://upload.wikimedia.org/wikipedia/commons/2/2c/Rotating_earth_%28large%29.gif',
+          duration: 30,
+          type: 'exercise',
+        },
+        {
+          title: 'rest',
+          display: 'https://upload.wikimedia.org/wikipedia/commons/2/2c/Rotating_earth_%28large%29.gif',
+          duration: 10,
+          type: 'break',
+        }
+      ],
+    };
+  }
+
+  render() {
+    const { roomName, displayName, password, workout } = this.state;
+    
+    return (
+      <div className="App full-size">
+        <ThemeProvider theme={theme}>
+          <Meeting
+            roomName={roomName}
+            displayName={displayName}
+            password={password}
+            workout={workout}
+          />
+        </ThemeProvider>
+      </div>
+    );
+  }
 }
 
 export default App;
--- frontend/x/src/meeting/Meeting.js
+++ frontend/x/src/meeting/Meeting.js
@@ -1,23 +1,43 @@
 import './Meeting.css';
 import '../styles.css';
+import React from 'react';
 import Display from './Display';
 import Timer from './Timer';
 import Preview from './Preview';
+import Timeline from './Timeline';
 
-function Meeting() {
-  return (
-    <div className="dark-background full-size">
-      <div className="full-size vert-flex">
-        <div className="flex-fill" id="jitsi-frame">
-        </div>
-        <div className="horiz-flex mid-background" id="bottom-bar">
-          <Display id="exercise-display" />
-          <Timer className="flex-fill" />
-          <Preview id="preview" />
+class Meeting extends React.Component {
+  constructor(props) {
+    super();
+
+    this.state = {
+      time: 15,
+      section: 0,
+      currentTimer: 0,
+    };
+  }
+
+  render() {
+    const { roomName, displayName, password, workout } = this.props;
+    const { time, section, currentTimer } = this.state;
+
+    return (
+      <div className="dark-background full-size">
+        <div className="full-size vert-flex">
+          <div className="light-background" id="timeline">
+            <Timeline workout={workout} time={time} length={workout.reduce((length, section) => length + section.duration, 0)} />
+          </div>
+          <div className="flex-fill" id="jitsi-frame">
+          </div>
+          <div className="horiz-flex mid-background" id="bottom-bar">
+            <Display gif={workout[section].display} />
+            <Timer time={currentTimer} />
+            <Preview title={(section >= workout.length) ? 'Finish workout' : workout[section].title} />
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Meeting;
